152cc1d1b1ef83f398e3aa9857b1bcc888a4a447
ayumi goto <ayumi.goto.1016@gmail.com>
2019-10-06 00:16:42 +0900

    Lesson7 課題
    キューブと地面、キューブ同士の衝突時に効果音をつけました。

---
+++ Assets/CrashSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+
+		AudioSource SE = GetComponent<AudioSource> ();
+
+		if (collision.gameObject.name == "ground" || collision.gameObject.name == "CubePrefab(Clone)" ) {
+
+			SE.Play();
+
+		} 
+}
+}
